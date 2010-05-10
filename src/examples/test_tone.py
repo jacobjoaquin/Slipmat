11d62f9a48c9f6f003ea812825b7532484fe0fdf
Jacob Joaquin <jacobjoaquin@gmail.com>
2010-05-10 14:32:49 -0700

    test_tone.py is as simple as possible.

--- src/examples/test_tone.py
+++ src/examples/test_tone.py
@@ -1,9 +1,23 @@
 #!/usr/bin/env python
+#
+# Copyright (C) 2010 Jacob Joaquin
+#
+# This file is part of slipmat.
+# 
+# slipmat is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# slipmat is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+# 
+# You should have received a copy of the GNU Lesser General Public License
+# along with slipmat.  If not, see <http://www.gnu.org/licenses/>.
 
 import slipmat
-
-slipmat.sr = 11025
-slipmat.ksmps = 25
 
 s = slipmat.ScoreEvents()
 s.event(0, 4, slipmat.Sine())
